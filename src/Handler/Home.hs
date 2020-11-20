9e18f99f393af0f12489932867dd40bc02623dbb
Lucas Viana <vianadossantoslucas@gmail.com>
2020-11-19 23:06:24 -0300

    :lipstick: adds basic style to homepage

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -7,24 +7,9 @@ module Handler.Home where
 
 import Import
 
--- Define our data that will be used for creating the form.
-data FileForm = FileForm
-    { fileInfo :: FileInfo
-    , fileDescription :: Text
-    }
-
--- This is a handler function for the GET request method on the HomeR
--- resource pattern. All of your resource patterns are defined in
--- config/routes.yesodroutes
---
--- The majority of the code you will write in Yesod lives in these handler
--- functions. You can spread them across multiple files if you are so
--- inclined, or create a single monolithic file.
 getHomeR :: Handler Html
 getHomeR = 
     defaultLayout $ do
-        setTitle "Welcome To Yesod!"
-
-postHomeR :: Handler Html
-postHomeR = defaultLayout $ do
-        setTitle "Welcome To Yesod!"
+        addStylesheet $ StaticR css_bootstrap_css
+        setTitle "BookRecorder - Home"
+        $(widgetFile "pages/homepage")
